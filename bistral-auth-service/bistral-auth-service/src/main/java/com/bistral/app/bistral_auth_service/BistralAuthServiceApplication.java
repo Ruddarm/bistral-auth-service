857b89cfa4ceacfdd42a608e40124393f2687fc5
Ruddarm <ruddarmmaurya@gmail.com>
2026-01-20 18:50:07 +0530

    initilise auth service

---
+++ bistral-auth-service/bistral-auth-service/src/main/java/com/bistral/app/bistral_auth_service/BistralAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bistral.app.bistral_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BistralAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BistralAuthServiceApplication.class, args);
+	}
+
+}
---
+++ bistral-auth-service/bistral-auth-service/src/test/java/com/bistral/app/bistral_auth_service/BistralAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bistral.app.bistral_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BistralAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
